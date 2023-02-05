07e2ebd5c2f1df24af203419433889e1337a462f
Murali Dhar Tirupati <murali.tirupati@ibm.com>
2023-02-05 17:05:16 +0530

    Testing on export

--- public/scripts/generate-bosh-vms-vitals.sh
+++ public/scripts/generate-bosh-vms-vitals.sh
@@ -1,3 +1,5 @@
 echo "I am calling"
 echo "I am from /Users/murali/Desktop/Groovy/DO180-apps/public/scripts/generate-bosh-vms-vitals.sh"
+printf %100s |tr " " "="
 echo $PAYLOAD_FILE_PATH
+printf %100s |tr " " "="

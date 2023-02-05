9578b46c7fb95b3b5f56e5affc8f9ef359e7cf26
Murali Dhar Tirupati <murali.tirupati@ibm.com>
2023-02-05 10:28:33 +0530

    new files added

--- public/scripts/prepare-env.sh
+++ public/scripts/prepare-env.sh
@@ -12,6 +12,13 @@ vm_vital_pay_load="$file_base_name.json"
 echo $file_base_name,$vm_vitals_file_name,$vm_vital_pay_load
 PAYLOAD_FILE_PATH="$GITHUB_WORKSPACE/generate-bosh-vms-vitals/$USER_ENV/$vm_vital_pay_load"
 echo "The PAY load file path is:: $PAYLOAD_FILE_PATH"
+sudo mkdir -p  "$GITHUB_WORKSPACE/generate-bosh-vms-vitals/$USER_ENV"
+sudo touch "$PAYLOAD_FILE_PATH"
+sudo chown -R $GITHUB_ACTOR:$GITHUB_ACTOR $GITHUB_WORKSPACE/generate-bosh-vms-vitals/'
+sh  ./public/scripts/generate-bosh-vms-vitals.sh
+# sudo find "/mnt/isharesafeway/reports/generate-bosh-vms-vitals/$USER_ENV/" -type d -name '*-*-*-*-*-*' -mtime +30 -print0 | xargs -0 -I {} rm -rf {} \\;
+# sudo mkdir -p "/mnt/isharesafeway/reports/generate-bosh-vms-vitals/$USER_ENV/$TODAY_DATE"
+# sudo cp ${PAYLOAD_FILE_PATH} "/mnt/isharesafeway/reports/generate-bosh-vms-vitals/$USER_ENV/$TODAY_DATE/$vm_vital_pay_load"
 
 # - name: set file path
 #         run: |
